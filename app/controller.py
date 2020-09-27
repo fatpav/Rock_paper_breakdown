0f7c0ef22fe5a13e7973f0ecda95c99fbac36caf
fatpav <fat_pav@hotmail.com>
2020-09-27 19:06:05 +0100

    lost.  No clue how to make my function take arguments

--- app/controller.py
+++ app/controller.py
@@ -1,7 +1,14 @@
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from tests.src.game import *
+from tests.src.player import *
+from app.models.game_choices import *
+
 
 @app.route('/')
 def index():
     return render_template('index.html', title = 'Home')
+
+@app.route('/players')
+def players():
+    return render_template('players.html')
---
+++ app/models/game_choices.py
@@ -0,0 +1,5 @@
+from tests.src.player import Player
+
+player1 = Player(input(), input())
+player2 = Player(input(), input())
+players = [player1.choice, player2.choice]
